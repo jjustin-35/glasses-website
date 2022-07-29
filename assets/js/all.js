a079bbafe9eddb20b5fca8423e0a1c9a3276616a
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-07-29 16:46:01 +0800

    Update 2022-07-29T08:45:45.043Z

---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+$('.burgerMenu').on('click', function () {
+  $('.menu').slideToggle();
+});
+//# sourceMappingURL=all.js.map
